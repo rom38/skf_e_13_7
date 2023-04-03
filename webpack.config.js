be9d87f3b7a3ccaab7c418e15389bc29b5576fa3
Rman vsc <rom38@users.noreply.github.com>
2023-04-03 21:22:22 +0800

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        hot: true,
+        devMiddleware: {
+            stats: {
+                children: false,
+                maxModules: 0,
+            },
+        },
+    }, 
+    plugins: [
+        // new HtmlWebpackPlugin({
+        //     title: 'Development',
+        // }),
+    ],
+    output: {
+        filename: 'main.js',
+    },
+};
